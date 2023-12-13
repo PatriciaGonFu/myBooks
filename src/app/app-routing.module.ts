97312da794318fdc1fa131a23ceb5a04d79a9389
PatriciaGonFu <patruskatrus@hotmail.com>
2023-12-13 17:45:55 +0100

    dia1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { RegisterComponent } from './page/register/register.component';
 
 const routes: Routes = [
-  // {path: "home", component: HomeComponent},
-  // {path: "register", component: RegisterComponent},
+  {path: '', component: HomeComponent},
+  {path: "home", component: HomeComponent},
+  {path: "register", component: RegisterComponent},
 ];
 
 @NgModule({
